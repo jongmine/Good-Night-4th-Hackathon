b460447f896731bd2cca767c4c8395d7ff8de101
jongmine <jongmine.ch@gmail.com>
2025-08-16 17:52:29 +0900

    set: Spring Boot 초기 설정

---
+++ backend/src/main/kotlin/com/gn4/seatreservation/SeatReservationApplication.kt
@@ -0,0 +1,11 @@
+package com.gn4.seatreservation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeatReservationApplication
+
+fun main(args: Array<String>) {
+    runApplication<SeatReservationApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/gn4/seatreservation/SeatReservationApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.gn4.seatreservation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SeatReservationApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
